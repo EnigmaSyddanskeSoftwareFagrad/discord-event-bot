6a78f064c8b4fd29612cb1e3aa812625dab868d9
jakob753951 <jlm1999@live.dk>
2023-04-29 15:24:21 +0200

    updated config

--- confmanager.py
+++ confmanager.py
@@ -1,11 +1,15 @@
+from dataclasses import dataclass
 import json
 
-
+@dataclass
 class ConfManager:
+    token: str
+    event_channel: int
+    enigma_role_id: int
+
     def __init__(self):
         with open('config/config.json') as config_file:
             self.config = json.load(config_file)
-        self.token = self.config["TOKEN"]
-        self.event_channel = self.config["EVENT_CHANNEL_ID"]
-        self.enigma_role_id = self.config["ENIGMA_ROLE_ID"]
-        self.bot_id = self.config["BOT_ID"]
+        self.token = self.config["token"]
+        self.event_channel = self.config["event_channel_id"]
+        self.enigma_role_id = self.config["enigma_role_id"]
