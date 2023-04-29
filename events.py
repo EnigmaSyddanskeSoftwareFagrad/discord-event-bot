f5aa8c6a5e604b4c138af270a54c7e6124356ef3
jakob753951 <jlm1999@live.dk>
2023-04-29 17:47:09 +0200

    serde fixes, field renaming, other fixing :)

--- events.py
+++ events.py
@@ -19,25 +19,41 @@ class EventNotFoundError(Exception):
 
 @dataclass
 class Event:
-    event_name: str
-    event_link: str
+    name: str
+    link: str
     organizer_id: int
     state: EventState = field(default=EventState.in_progress)
     description: str | None = field(default=None)
     uuid: int = field(default=uuid.uuid4().int)
 
+    def __eq__(self, other: 'Event') -> bool:
+        return self.name == other.name
+
     @staticmethod
     def from_dict(data: dict) -> 'Event':
         uuid = data['uuid']
-        event_name = data['event_name']
-        event_link = data['event_link']
+        event_name = data['name']
+        event_link = data['link']
         organizer_id = data['organizer_id']
         state = EventState(data['state'])
         description = data.get('description', None)
         return Event(event_name, event_link, organizer_id, state, description)
 
+    def to_dict(self) -> dict:
+        return {
+            'uuid': self.uuid,
+            'name': self.name,
+            'link': self.link,
+            'organizer_id': self.organizer_id,
+            'state': self.state.value,
+            'description': self.description
+        }
+
     def __str__(self):
-        return f"***{self.event_name}***\n{self.description}\n\nLink: {self.event_link}"
+        return f"***{self.name}***\n{self.description}\n\nLink: {self.link}"
+
+    def __hash__(self) -> int:
+        return id(self)
 
 class EventManager(MutableSet):
     def __enter__(self) -> 'EventManager':
@@ -50,44 +66,50 @@ class EventManager(MutableSet):
     def __contains__(self, event: Event) -> bool:
         return event in self.events
 
-    def get_event(self, event_name: str, user_id: int | None = None) -> Event | None:
-        if user_id is not None:
-            matches_event = lambda event: event["user_id"] == user_id and event["event_name"] == event_name
-        else:
-            matches_event = lambda event: event["event_name"] == event_name
-        return find(self.events, matches_event)
+    def __iter__(self):
+        return iter(self.events)
 
-    def add_event(self, event: Event):
-        # if the event already exists, raise error
-        if self.get_event(event.event_name, event.organizer_id) is not None:
+    def __len__(self) -> int:
+        return len(self.events)
+
+    def __getitem__(self, event_name: str) -> Event:
+        matches_event = lambda event: event.name == event_name
+        event = find(self.events, matches_event)
+        if event is not None:
+            return event
+        else:
+            raise EventNotFoundError()
+
+    def add(self, event: Event):
+        try:
+            self[event.name]
+        except EventNotFoundError:
+            self.events.add(event)
+        else:
             raise DuplicateEventError()
 
-        self.events.add(event)
+    def edit_event(self, event_name: str, user_id: int, event_link: str):
+        try:
+            self.remove(event_name)
+        except EventNotFoundError:
+            pass
 
-    def override_event(self, event_name: str, user_id: int, event_link: str):
-        event = self.get_event(event_name, user_id)
-        if event is not None:
-            self.events.remove(event)
+        self.add(Event(event_name, event_link, user_id))
 
-        self.add_event(Event(event_name, event_link, user_id))
+    def discard(self, event_name: str):
+        event = self[event_name]
+        return self.events.discard(event)
 
-    def get_event_or_error(self, event_name: str, user_id: int | None = None) -> Event:
-        event = self.get_event(event_name, user_id)
-        if event is None:
-            raise EventNotFoundError
-        else:
-            return event
-
-    def remove_event(self, event_name: str, user_id: int | None = None):
-        event = self.get_event_or_error(event_name, user_id)
+    def remove(self, event_name: str) -> None:
+        event = self[event_name]
         self.events.remove(event)
 
-    def set_event_description(self, event_name: str, user_id: int, event_description: str):
-        event = self.get_event_or_error(event_name, user_id)
+    def set_event_description(self, event_name: str, event_description: str):
+        event = self[event_name]
         event.description = event_description
 
-    def submit_event(self, user_id: int, event_name: str):
-        event = self.get_event_or_error(event_name, user_id)
+    def submit_event(self, event_name: str):
+        event = self[event_name]
         event.state = EventState.submitted
 
 
@@ -108,7 +130,7 @@ def save_events(events: set[Event]):
     # create parent folders if they don't exist
     os.makedirs(EVENTS_FILE_PATH, exist_ok=True)
     with open(EVENTS_FILE_NAME, 'w+') as events_file:
-        json.dump(events, events_file)
+        json.dump([event.to_dict() for event in events], events_file, indent=4)
 
 
 if __name__ == '__main__':
