f250e6faeab7380f83b9b98ca9d947d9b1111b07
jakob753951 <jlm1999@live.dk>
2023-04-29 15:24:35 +0200

    more config

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import confmanager
 import eventcreator
 from eventcreator import Event
 
-disc_values = confmanager.ConfManager()
-bot = lightbulb.BotApp(token=disc_values.token)
+config = confmanager.ConfManager()
+bot = lightbulb.BotApp(token=config.token)
 
 
 @bot.listen()
@@ -34,12 +34,15 @@ async def on_event_description(event: message_events.DMMessageCreateEvent) -> No
     print(message.content)
     if message.content is None:
         return
-    if message.content.startswith(f'<@{disc_values.bot_id}> name:'):
+    me = bot.get_me()
+    if me is None:
+        return
+    if message.content.startswith(f'<@{me.id}> name:'):
         pattern = re.compile(r"^<@\d+> name:(?P<event_name>.*?) description:")
         match = pattern.match(message.content)
         if match:
             event_name = match.group("event_name")
-            description = message.content.replace(f"<@{disc_values.bot_id}> name:{event_name} description:", "")
+            description = message.content.replace(f"<@{me.id}> name:{event_name} description:", "")
             eventcreator.set_event_description(event_name, message.author.id, description)
             await message.author.send("Thank you for using \"Enigma Event Bot\"! "
                                             "Your event will be added shortly...")
@@ -64,14 +67,17 @@ async def on_event_post(event: message_events.DMMessageCreateEvent) -> None:
     print(message.content)
     if message.content is None:
         return
-    if message.content.startswith(f'<@{disc_values.bot_id}> post name:'):
+    me = bot.get_me()
+    if me is None:
+        return
+    if message.content.startswith(f'<@{me.id}> post name:'):
         pattern = re.compile(r"^<@\d+> post name:\s*(?P<event_name>\S.*)$")
         match = pattern.match(message.content)
         if match is not None:
             event_name = match.group("event_name")
             new_event: Event = eventcreator.get_event(event_name, message.author.id)
             print("event name", new_event.event_name)
-            event_channel = disc_values.event_channel
+            event_channel = config.event_channel
             await bot.rest.create_message(event_channel, new_event)
             await message.author.send("Your event has been posted!")
             eventcreator.submit_event(message.author.id, event_name)
@@ -80,14 +86,14 @@ async def on_event_post(event: message_events.DMMessageCreateEvent) -> None:
 
 
 @bot.command
-@lightbulb.add_checks(lightbulb.has_roles(disc_values.enigma_role_id))
+@lightbulb.add_checks(lightbulb.has_roles(config.enigma_role_id))
 @lightbulb.option("event_name", "Name of the event", str)
 @lightbulb.option("event_link", "Link to facebook event", str)
 @lightbulb.command('addevent', 'start an event submission')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def add_event(ctx: lightbulb.SlashContext):
     print("add event")
-    event_channel = disc_values.event_channel
+    event_channel = config.event_channel
     await bot.rest.create_message(event_channel, f"**{ctx.options.event_name}**\n{ctx.options.event_link}")
     await ctx.respond(ctx.options.event_name)
     try:
